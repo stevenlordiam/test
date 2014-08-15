9267bdb3f64097deacc838c8007188b7bb0fc2fd
stevenlordiam <stevenlordiam@gmail.com>
2014-08-15 12:50:02 -0400

    test

--- run.sh
+++ run.sh
@@ -1,10 +1,4 @@
-for month in 08 09 10 11 12
-do
-	for day in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
-	do
-	sudo date $month$day125014 
-	echo test $month$day >> test.txt
+
+	echo test  >> test.txt
 	git add .
 	git commit -m "test"
-done
-done
