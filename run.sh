413e76bd0a813116439ed9b455cccd481e0f08df
stevenlordiam <stevenlordiam@gmail.com>
2014-08-18 12:08:00 -0400

    test

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
-echo test >> test.txt
-git add .
-git commit -m "test"
+for month in 08 09 10 11 12
+do
+	for day in 01 02 03 ... 30
+	do
+	sudo date $month$day125014 
+	echo test >> test.txt
+	git add .
+	git commit -m "test"
+done
+done
